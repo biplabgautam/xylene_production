acba1f66cb6232827739e73ab88ab6543473976f
BiplabG <071bex411.biplab@pcampus.edu.np>
2018-02-15 12:44:37 -0500

    Changed settings file for postgres

--- settings.py
+++ settings.py
@@ -86,15 +86,10 @@ WSGI_APPLICATION = 'xylene.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lmphdb',
-        'USER': 'biplab',
-        'PASSWORD': 'biplab.gautam.411',
-        'HOST': '139.59.77.203',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
@@ -158,3 +153,21 @@ MARKDOWN_DEUX_STYLES = {
 }
 
 LOGIN_URL = '/login'
+
+# Heroku settings
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+    }
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Allow all host headers.
+    ALLOWED_HOSTS = ['*']
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+        )
+    MEDIA_ROOT = 'media'
